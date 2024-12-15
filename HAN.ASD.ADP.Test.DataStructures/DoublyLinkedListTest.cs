525addf81529e93f3e0c91894081330239d62d2c
Rowan <r.kooij17@gmail.com>
2024-12-15 21:24:52 +0100

    DLList testen begin

--- HAN.ASD.ADP.Test.DataStructures/DoublyLinkedListTest.cs
+++ HAN.ASD.ADP.Test.DataStructures/DoublyLinkedListTest.cs
@@ -1,7 +1,34 @@
-﻿namespace HAN.ASD.ADP.Test.DataStructures
+﻿using HAN.ASD.ADP.DataStructures.DLList;
+using System.Diagnostics;
+
+namespace HAN.ASD.ADP.Test.DataStructures
 {
     [TestClass]
     public class DoublyLinkedListTest
     {
+        private Stopwatch Watch = new Stopwatch();
+
+        [TestMethod]
+        public void Adding10Deleting10()
+        {
+            var dlist = new DoublyLinkedList<int>();
+            Watch.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                dlist.Add(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine(Watch.ToString());
+
+            Watch.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                dlist.Remove(element: i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine(Watch.ToString());
+        }
     }
 }
