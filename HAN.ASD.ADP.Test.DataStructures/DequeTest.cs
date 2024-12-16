02c65f38ed51fcfd24dae52ff9b4b9eafc06ea24
Rowan <r.kooij17@gmail.com>
2024-12-16 15:14:13 +0100

    Performance test deque

--- HAN.ASD.ADP.Test.DataStructures/DequeTest.cs
+++ HAN.ASD.ADP.Test.DataStructures/DequeTest.cs
@@ -9,7 +9,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         private Stopwatch Watch = new Stopwatch();
 
         [TestMethod]
-        public void InsertRight_DeleteLeft_NotPropperSize()
+        public void InsertRightDeleteLeft_NotPropperSize()
         {
             var deque = new Deque<int>();
             Watch.Restart();
@@ -33,7 +33,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertRight_DeleteRight_NotPropperSize()
+        public void InsertRightDeleteRight_NotPropperSize()
         {
             var deque = new Deque<int>();
             Watch.Restart();
@@ -57,7 +57,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertRight_DeleteLeft_PropperSize()
+        public void InsertRightDeleteLeft_PropperSize()
         {
             var deque = new Deque<int>(10);
             Watch.Restart();
@@ -81,9 +81,9 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertRight_DeleteRight_PropperSize()
+        public void InsertRightDeleteRight_PropperSize()
         {
-            var deque = new Deque<int>();
+            var deque = new Deque<int>(10);
             Watch.Restart();
             for (int i = 0; i < 10; i++)
             {
@@ -105,7 +105,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertLeft_DeleteRight_NoPropperSize()
+        public void InsertLeftDeleteRight_NoPropperSize()
         {
             var deque = new Deque<int>();
             Watch.Restart();
@@ -129,7 +129,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertLeft_DeleteLeft_NoPropperSize()
+        public void InsertLeftDeleteLeft_NoPropperSize()
         {
             var deque = new Deque<int>();
             Watch.Restart();
@@ -153,7 +153,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertLeft_DeleteRight_PropperSize()
+        public void InsertLeftDeleteRight_PropperSize()
         {
             var deque = new Deque<int>(10);
             Watch.Restart();
@@ -184,7 +184,7 @@ namespace HAN.ASD.ADP.Test.DataStructures
         }
 
         [TestMethod]
-        public void InsertLeft_DeleteLeft_PropperSize()
+        public void InsertLeftDeleteLeft_PropperSize()
         {
             var deque = new Deque<int>(10);
             Watch.Restart();
@@ -205,6 +205,205 @@ namespace HAN.ASD.ADP.Test.DataStructures
             Watch.Stop();
 
             Console.WriteLine($"Deleting 10x: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertLeft100000DeletingLeft_PropperSize()
+        {
+            var deque = new Deque<int>(100000);
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertLeft(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            while (deque.Size() != 0)
+            {
+                var deleted = deque.DeleteLeft();
+                Assert.IsTrue(deleted == deque.Size());
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertLeft100000DeleteRight_PropperSize()
+        {
+            var deque = new Deque<int>(100000);
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertLeft(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 10x: {Watch}");
+
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                var deleted = deque.DeleteRight();
+                if (i == 0)
+                {
+                    Assert.IsTrue(deleted == 100000 - 1);
+                }
+                else
+                {
+                    Assert.IsTrue(deleted == i);
+                }
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 10x: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertLeft100000DeletingLeft_NotPropperSize()
+        {
+            var deque = new Deque<int>();
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertLeft(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            while (deque.Size() != 0)
+            {
+                var deleted = deque.DeleteLeft();
+                Assert.IsTrue(deleted == deque.Size());
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertLeft100000DeleteRight_NotPropperSize()
+        {
+            var deque = new Deque<int>();
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertLeft(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                var deleted = deque.DeleteRight();
+                Assert.IsTrue(deleted == i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertRight100000DeleteLeft_NotPropperSize()
+        {
+            var deque = new Deque<int>();
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertRight(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                var deleted = deque.DeleteLeft();
+                Assert.IsTrue(deleted == i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertRight100000DeleteRight_NotPropperSize()
+        {
+            var deque = new Deque<int>();
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertRight(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            while (deque.Size() != 0)
+            {
+                var deleted = deque.DeleteRight();
+                Assert.IsTrue(deleted == deque.Size());
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertRight100000DeleteLeft_PropperSize()
+        {
+            var deque = new Deque<int>(100000);
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertRight(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                var deleted = deque.DeleteLeft();
+                Assert.IsTrue(deleted == i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
+        }
+
+        [TestMethod]
+        public void PerformanceTest_InsertRight100000DeleteRight_PropperSize()
+        {
+            var deque = new Deque<int>(100000);
+            Watch.Restart();
+            for (int i = 0; i < 100000; i++)
+            {
+                deque.InsertRight(i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Inserting 100000: {Watch}");
+
+            Watch.Restart();
+            while (deque.Size() != 0)
+            {
+                var deleted = deque.DeleteRight();
+                Assert.IsTrue(deleted == deque.Size());
+            }
+            Watch.Stop();
+
+            Console.WriteLine($"Deleting 100000: {Watch}");
         }
     }
 }
