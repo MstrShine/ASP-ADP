e4a4557f26915ff188a4da4c3bb3dda743a6329c
Rowan <r.kooij17@gmail.com>
2024-12-16 16:30:32 +0100

    Stack tests

--- HAN.ASD.ADP.Test.DataStructures/PriorityQueueTest.cs
+++ HAN.ASD.ADP.Test.DataStructures/PriorityQueueTest.cs
@@ -1,7 +1,10 @@
-﻿namespace HAN.ASD.ADP.Test.DataStructures
+﻿using System.Diagnostics;
+
+namespace HAN.ASD.ADP.Test.DataStructures
 {
     [TestClass]
     public class PriorityQueueTest
     {
+        private Stopwatch Watch = new Stopwatch();
     }
 }
--- HAN.ASD.ADP/DataStructures/Stack/IADPStack.cs
+++ HAN.ASD.ADP/DataStructures/Stack/IADPStack.cs
@@ -1,6 +1,6 @@
 ﻿namespace HAN.ASD.ADP.DataStructures.Stack
 {
-    public interface IStack<T> where T : IComparable<T>
+    public interface IADPStack<T> where T : IComparable<T>
     {
         void Push(T item);
 
