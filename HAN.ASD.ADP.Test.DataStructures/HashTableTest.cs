2547a9e12599a87cc2e7a0c6d5d3234a27649fe7
Rowan <r.kooij17@gmail.com>
2025-01-07 15:06:09 +0100

    Start test

---
+++ HAN.ASD.ADP.Test.DataStructures/HashTableTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace HAN.ASD.ADP.Test.DataStructures
+{
+    [TestClass]
+    public class HashTableTest
+    {
+        private Stopwatch Watch = new Stopwatch();
+
+        [TestMethod]
+        public void Insert10Linear()
+        {
+            var table = new ADP.DataStructures.HashTable.Linear.HashTable<int>();
+            Watch.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                table.Insert(i.ToString(), i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine(Watch.ToString());
+        }
+
+        [TestMethod]
+        public void Insert10Quad()
+        {
+            var table = new ADP.DataStructures.HashTable.Quadratic.HashTable<int>();
+            Watch.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                table.Insert(i.ToString(), i);
+            }
+            Watch.Stop();
+
+            Console.WriteLine(Watch.ToString());
+        }
+    }
+}
